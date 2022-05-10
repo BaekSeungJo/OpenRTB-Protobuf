21400f58e614709df583628c123adc6ad237c0c5
BAEK SEUNG JO <sjbaek@enliple.com>
2022-05-10 21:02:40 +0900

    Protocol Buffer 기본 문서 및 테스트 코드 작성 완료

---
+++ src/test/java/com/example/protobuf/addressbook/PersonTest.java
@@ -0,0 +1,48 @@
+package com.example.protobuf.addressbook;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PersonTest {
+
+  @Test
+  public void Person_생성_테스트() {
+    // given
+    int id = 1;
+    String name = "Baek SeungJo";
+    String phoneNumber = "1234-5678";
+    Person.PhoneType phoneType = Person.PhoneType.HOME;
+
+    // when
+    Person person = Person.newBuilder()
+      .setId(id)
+      .setName("Baek SeungJo")
+      .addPhones(
+        Person.PhoneNumber.newBuilder()
+          .setNumber("1234-5678")
+          .setType(phoneType))
+      .build();
+
+    // then
+    assertThat(person.getId()).isEqualTo(id);
+    assertThat(person.getName()).isEqualTo(name);
+    assertThat(person.getPhonesCount()).isEqualTo(1);
+    assertThat(person.getPhones(0).getNumber()).isEqualTo(phoneNumber);
+    assertThat(person.getPhones(0).getType()).isEqualTo(phoneType);
+  }
+
+  @Test
+  public void Person_required_필드_설정_검증_테스트() {
+    // given
+    String name = "Baek SeungJo";
+
+    // when
+    Person person = Person.newBuilder()
+      .setName("Baek SeungJo")
+      .build();
+
+    // then
+    assertThat(person.isInitialized()).isEqualTo(true);
+  }
+}
